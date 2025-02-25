50f5ea7595435a92ed38e72a19a98101480a5cb6
Corin Staves <corin.staves@tum.de>
2025-02-25 16:15:00 +0100

    Updated destination choice scripts

--- script/Manchester/destinationChoice/destinations.R
+++ script/Manchester/destinationChoice/destinations.R
@@ -3,33 +3,32 @@
 library(tidyverse)
 
 destinations <- list()
-destinations$community_health <- read_delim("data/Manchester/destinations/Community_health_resources.csv", delim = ";", col_types = "innnic")
-destinations$early_year_access <- read_delim("data/Manchester/destinations/Early_year_access.csv", delim = ";", col_types = "innnic")
-destinations$eating_establishments <- read_delim("data/Manchester/destinations/Eating_establishments.csv", delim = ";", col_types = "innnic")
-destinations$education <- read_delim("data/Manchester/destinations/Education.csv", delim = ";", col_types = "innnic")
-destinations$financial <- read_delim("data/Manchester/destinations/Financial.csv", delim = ";", col_types = "innnic")
-destinations$food_retail <- read_delim("data/Manchester/destinations/Food_retail.csv", delim = ";", col_types = "innnic")
-destinations$primary_health_care <- read_delim("data/Manchester/destinations/Primary_health_care.csv", delim = ";", col_types = "innnic")
-destinations$recreation <- read_delim("data/Manchester/destinations/Recreational_sports_pitches_and_facilities.csv", delim = ";", col_types = "innnic")
-destinations$services <- read_delim("data/Manchester/destinations/Services.csv", delim = ";", col_types = "innnic")
-destinations$social_and_culture <- read_delim("data/Manchester/destinations/Social_and_culture_locations.csv", delim = ";", col_types = "innnic")
+destinations$community_health <- read_delim("../../manchester/destinations/Community_health_resources.csv", delim = ";", col_types = "innnic")
+destinations$early_year_access <- read_delim("../../manchester/destinations/Early_year_access.csv", delim = ";", col_types = "innnic")
+destinations$eating_establishments <- read_delim("../../manchester/destinations/Eating_establishments.csv", delim = ";", col_types = "innnic")
+destinations$education <- read_delim("../../manchester/destinations/Education.csv", delim = ";", col_types = "innnic")
+destinations$financial <- read_delim("../../manchester/destinations/Financial.csv", delim = ";", col_types = "innnic")
+destinations$food_retail <- read_delim("../../manchester/destinations/Food_retail.csv", delim = ";", col_types = "innnic")
+destinations$primary_health_care <- read_delim("../../manchester/destinations/Primary_health_care.csv", delim = ";", col_types = "innnic")
+destinations$recreation <- read_delim("../../manchester/destinations/Recreational_sports_pitches_and_facilities.csv", delim = ";", col_types = "innnic")
+destinations$services <- read_delim("../../manchester/destinations/Services.csv", delim = ";", col_types = "innnic")
+destinations$social_and_culture <- read_delim("../../manchester/destinations/Social_and_culture_locations.csv", delim = ";", col_types = "innnic")
 
-destinations$public_open_space <- read_delim("data/Manchester/destinations/Public_open_space_v4.0.csv", delim = ";") %>%
+destinations$public_open_space <- read_delim("../../manchester/destinations/Public_open_space_v4.0.csv", delim = ";") %>%
   group_by(ID) %>% 
   mutate(ID = cur_group_id()) %>% 
   ungroup() %>%
   transmute(ID,X,Y,WEIGHT,ref_no = as.integer(0),name = "unknown")
 
-destinations$public_transport <- read_delim("data/Manchester/destinations/Public_transport.csv", delim = ";", col_types = "innni") %>% mutate(ref_no = as.integer(0),name = "unknown")
+destinations$public_transport <- read_delim("../../manchester/destinations/Public_transport.csv", delim = ";", col_types = "innni") %>% mutate(ref_no = as.integer(0),name = "unknown")
 
 all_destinations <- bind_rows(destinations, .id = "type") %>% mutate(ID = paste(type,ID,sep = "_"))
-write_delim(all_destinations,"data/Manchester/destinations/all.csv",delim = ";")
 
-# USE REFORMATTED WEIGHT DISTRIBUTIONS
-all_destinations %>%
-  mutate(WEIGHT2 = WEIGHT ^ 0.5,
-         WEIGHT4 = WEIGHT ^ 0.25) %>%
-  write_delim("destinations/all.csv",delim = ";")
+# # TEST & WRITE DIFFERENT TRANSFORMATIONS
+# all_destinations %>%
+#   mutate(WEIGHT2 = WEIGHT ^ 0.5,
+#          WEIGHT4 = WEIGHT ^ 0.25) %>%
+#   write_delim("../../manchester/destinations/all_test.csv",delim = ";")
 
 ## PLOT WEIGHT DISTRIBUTIONS ##
 all_destinations <- all_destinations %>% 
@@ -46,15 +45,36 @@ all_destinations <- all_destinations %>%
                                `services` = "Services (α = 0.5)",
                                `public_transport` = "Public transport (α = 0.5)",
                                `public_open_space` = "Public open space (α = 0.25)",.ordered = TRUE),
+         code = recode_factor(type,
+                              `social_and_culture` = "SCL",
+                              `education` = "EDU",
+                              `primary_health_care` = "PHC",
+                              `community_health` = "CHR",
+                              `recreation` = "RSPF",
+                              `early_year_access` = "EYA",
+                              `food_retail` = "FR",
+                              `eating_establishments` = "EE",
+                              `financial` = "FIN",
+                              `services` = "SER",
+                              `public_transport` = "PT",
+                              `public_open_space` = "POS"),
          WT = case_when(type == "public_open_space" ~ WEIGHT ^ 0.25,
                         type == "recreation" ~ WEIGHT ^ 0.25,
                         type == "social_and_culture" ~ WEIGHT ^ 0.25,
                         type == "education" ~ WEIGHT,
-                        TRUE ~ WEIGHT ^ 0.5)) 
+                        TRUE ~ WEIGHT ^ 0.5)) %>%
+  relocate(code,.after = "type") %>%
+  relocate(title,.after = "code")
 
+# Plot distribution of transformed weights
 ggplot(all_destinations,aes(x = WT)) + 
   geom_density() + facet_wrap(~title, scales = "free") + ylab("Density") + xlab("Weight ^ α") +
   ggtitle("Destination weights with power transform α", subtitle = "Kernel density estimations")
 
-all_destinations <- all_destinations %>% group_by(type) %>% mutate(WT_NORM = WT / max(WT)) %>% ungroup()
-write_delim(all_destinations,"data/Manchester/destinations/all.csv",delim = ";")
+# Write file with normalised weights for easier visualisation
+norm_destinations <- all_destinations %>%
+  group_by(type) %>% 
+  mutate(WT_NORM = WT / max(WT)) %>% 
+  ungroup()
+  
+write_delim(norm_destinations,"../../manchester/destinations/all.csv",delim = ";")
